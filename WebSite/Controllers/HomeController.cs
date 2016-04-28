a8dde36b0dfb28573dedf8fbbb5cfad3789acc2b
matthias <matthias@2410-0238>
2016-04-28 08:39:17 +0200

    Website Create

---
+++ WebSite/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Information = "Das ist der erste Text der MVC-Applikation!";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+    }
+}
